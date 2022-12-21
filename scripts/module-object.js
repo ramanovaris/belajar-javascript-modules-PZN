ca793476e2876079cd81de3712044c782955d7ee
Rama Novaris Ayyubi Pratama <ramaanovariss@gmail.com>
2022-12-21 08:36:59 +0800

    Module Object

---
+++ scripts/module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Rama";
+export const middleName = "Novaris";
+export const lastName = "Ayyubi";
+
+export function hello() {
+  console.info("Hello from Rama");
+}
+
+export class Person {
+  constructor() {
+    this.name = "Rama";
+  }
+}
